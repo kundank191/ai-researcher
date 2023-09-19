e0c5fb3390c5a3e89d8e79baebbef1d9c71eb4e1
Kundan <kundank191@gmail.com>
2023-09-19 14:55:38 +0530

    create a model and connect all the tools

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from pydantic import BaseModel, Field
 
 import requests
 import streamlit as st
+from typing import Type
 
 # Setup logging
 log_format = '%(asctime)s - %(levelname)s - %(filename)s: %(message)s'
@@ -131,3 +132,72 @@ def summarize(objective, content):
     )
 
     return ouput
+
+class ScrapeWebsiteInput(BaseModel):
+    """Inputs for scrape_website"""
+    objective : str = Field(description = "The objective and task that users give to the agent")
+    url : str = Field(description = "The URL of the website to be scraped")
+
+class ScrapeWebsiteTool(BaseModel):
+    """This tool will run the scrape website and return the results"""
+
+    name = "scrape_website"
+    description = "useful when you need to get the data from a website url; passing both the url and the objective to the function; Donot Make up any url; Donot make up any url"
+    args_schema: Type[BaseModel] = ScrapeWebsiteInput
+
+    def _run(self, objective : str, url : str):
+        return scrape_website(objective, url)
+
+    def _arun(sel, url: str):
+        raise NotImplementedError("Not Implemented yet")
+    
+# Create a Langchain Agents with the tools above
+tools = [
+    Tool(
+        name = "Search",
+        func = search,
+        description = "useful for when you need to answer questions about current events, data. You should ask targeted questions"
+    ),
+    ScrapeWebsiteTool()
+]
+
+system_message = SystemMessage(
+    content = """ You are a world class researcher, who can do detailed research on any topic and produce facts based results;
+    you donot make things up, you will try as hard as possible to gather facts and data to back up the research
+
+    Please make sure you complete the objective with the following rules
+    1/ You should do enough research to gather as much information as possible about the objective
+    2/ If there are url of the relevant links and articles, you will scrape it to gather more information
+    3/ After scraping and search, you should think "is there any new things I should search or scrape based on the data I collected to increase research quality?" if the answer is yes, continue; But dont do this more than 3 iterations
+    4/ You should not make things up, you should only write facts and data that you have gathered
+    5/ In the final outpul, you should include all the reference data and links to back up your research; You should include all references
+    6/ In the final outpul, you should include all the reference data and links to back up your research; You should include all references
+
+    """
+)
+
+agent_kwargs = {
+    "extra_prompt_messages" : [MessagesPlaceholder(variable_name = "memory")],
+    "system_message" : system_message
+}
+
+llm = ChatOpenAI(
+    temperature = 0,
+    model = "gpt-3.5-turbo-16k-0613"
+)
+
+memory = ConversationSummaryBufferMemory(
+    memory_key = "memory",
+    return_messages = True,
+    llm = llm,
+    max_token_limit = 1000
+)
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent = AgentType.OPENAI_FUNCTIONS,
+    verbose = True,
+    agent_kwargs = agent_kwargs,
+    memory = memory
+)
